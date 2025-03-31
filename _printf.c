cf41ccc506559ca491e90009651e9af15ee0d4fa
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-03-31 12:15:54 +0000

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,9 +1,39 @@
-#ifndef HEADER
-#define HEADER
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _printf - A simplified version of printf
+ * @format: The format string containing
+ * the characters and the format specifiers
+ *
+ * Return: The length of the output
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0;
+	unsigned int len = 0;
+	va_list args;
 
-int _putchar(char c);
-int print_string(char *ptr);
-int print_number(int num);
-int _printf(const char *format, ...);
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] == 'c')
+			len += _putchar(va_arg(args, int)), i += 2;
 
-#endif
+		else if (format[i] == '%' && format[i + 1] == 's')
+			len += print_string(va_arg(args, char *)), i += 2;
+
+		else if (format[i] == '%' && format[i + 1] == '%')
+			len += _putchar('%'), i += 2;
+
+		else if (format[i] == '%' && format[i + 1] == '\0')
+			len--, i++;
+
+		else if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+			len += print_number(va_arg(args, int)), i += 2;
+
+		else
+			len += _putchar(format[i]), i++;
+	}
+	va_end(args);
+	return (len);
+}
