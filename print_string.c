2aa35e746f599015a7c7bde79500853ca2bf4fe3
tamerlanhtm <tamerlan_htm@hotmail.com>
2025-04-03 08:52:48 +0000

    print_string.c

--- print_string.c
+++ print_string.c
@@ -1,18 +1,25 @@
 #include "main.h"
-
+/**
+ * print_string - Prints a string to the standard output
+ * @ptr: Pointer to the string to be printed
+ *
+ * Return: The length of the string
+ */
 int print_string(char *ptr)
 {
-	int len;
+	int len = 0;
 
-	if (ptr == 0)
-		return;
+	if (ptr == (char *)0)
+	{
+		print_string("(null)");
+		return (6);
+	}
 
 	while (ptr[len])
 	{
-		_putchar(*ptr);
-		ptr++;
+		_putchar(ptr[len]);
+		len++;
 	}
 
-	return len;
+	return (len);
 }
-
